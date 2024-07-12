3e64861492a37fe0449f43095ccf237c2489809f
kimminsum <tommy1005a@gmail.com>
2024-07-13 08:27:17 +0900

    Add the tage of the stock

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from naver_api import NaverAPI
 from upload_to_database import UploadToDataBase
 
 
-for subject in ['Economy', 'Science', 'Society', 'Politics']:
+for subject in ['Economy', 'Science', 'Society', 'Politics', 'Stock']:
     naver_api = NaverAPI(subject, 100)
     news_data = naver_api.parse_data(naver_api.get_news())
 

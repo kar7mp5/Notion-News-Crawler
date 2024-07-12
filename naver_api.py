63a9da432c01038d80ead4dc62027629ab6c9f29
kimminsum <tommy1005a@gmail.com>
2024-07-13 05:40:59 +0900

    Add the fuction of parse_data; parsing the data that can use in upload_to_database.py

--- naver_api.py
+++ naver_api.py
@@ -9,9 +9,9 @@ class NaverAPI(Config):
     def __init__(self, subject, news_num):
         """NaverAPI
         
-        Arg:
+        Args:
             subject (str): The subject of the article
-            news_num (int): News number
+            news_num (int): Number of news articles to retrieve
         """
         super().__init__()
         
@@ -24,17 +24,23 @@ class NaverAPI(Config):
             "X-Naver-Client-Secret": self.x_naver_secret
         }
 
+        self.tags = {
+            'Economy': '경제',
+            'Science': '과학',
+            'Society': '사회',
+            'Politics': '정치'
+        }
+
 
     def clean_text(self, text):
-        """Delete html tags
+        """Delete HTML tags from the text
         
-        Arg:
+        Args:
             text (str): The text of the article
             
         Returns:
-            str: The text that don't have html tags
+            str: The text without HTML tags
         """
-
         clean = re.compile('<.*?>')
         text_without_tags = re.sub(clean, '', text)
 
@@ -42,31 +48,57 @@ class NaverAPI(Config):
 
 
     def get_news(self):
-        """Get news
+        """Get news articles from Naver API
 
         Returns:
-            list[dict[str]]: The result of the feature; ['title', 'originallink', 'link', 'description', 'pubDate']
+            dict: The result containing a list of news articles with keys ['title', 'originallink', 'link', 'description', 'pubDate'], and the subject
         """
-        
         # Define the URL and query parameters
         url = "https://openapi.naver.com/v1/search/news.json"
         params = {
-            "query": self.subject,
+            "query": self.tags[self.subject],
             "display": self.news_num,
             "start": 1,
             "sort": "sim"
         }
-        
+
         # Make the request
         response = requests.get(url, headers=self.headers, params=params)
-
-        for item in response.json()['items']:
+        response = response.json()
+        
+        for item in response['items']:
             item['title'] = self.clean_text(item['title'])
             item['description'] = self.clean_text(item['description'])
 
-        return response.json()['items']
+        return {
+            'contents': response['items'], 
+            'tags': self.subject
+        }
+
+
+    def parse_data(self, data):
+        """Parse the news data
+
+        Args:
+            data (dict): The data containing news articles and tags
+
+        Returns:
+            list: A list of parsed news articles with keys ['name', 'description', 'tag']
+        """
+        result = []
+        tag = data['tags']
+
+        for item in data['contents']:
+            result.append({
+                            'name': item['title'], 
+                            'description': item['description'],
+                            'tag': tag
+                        })
+        
+        return result
+
 
 
 if __name__=='__main__':
-    naver_api = NaverAPI('뉴스', 10)
-    print(naver_api.get_news())
+    naver_api = NaverAPI('Economy', 10)
+    print(naver_api.parse_data(naver_api.get_news()))
